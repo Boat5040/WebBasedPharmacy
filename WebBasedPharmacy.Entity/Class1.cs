19745262f74c4c591f9731ccdcf9f618647e1b81
Kwasi Boateng <Kwasi Boateng@KWASI>
2020-03-25 13:49:27 -0400

    Add project files.

---
+++ WebBasedPharmacy.Entity/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebBasedPharmacy.Entity
+{
+    public class Class1
+    {
+    }
+}
---
+++ WebBasedPharmacy.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebBasedPharmacy.Services
+{
+    public class Class1
+    {
+    }
+}
